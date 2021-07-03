e4e3d4515a83a639657f56e020368b4ee100dc28
加藤智之 <katotomoyuki@katoutoshiyukinoMacBook-Pro-2.local>
2021-07-03 22:23:49 +0900

    update controller

--- app/controllers/api/v1/line_foods_controller.rb
+++ app/controllers/api/v1/line_foods_controller.rb
@@ -1,8 +1,7 @@
 module Api
   module V1
     class LineFoodsController < ApplicationController
-      before_action :set_food, only: %i[create]
-      
+      before_action :set_food, only: %i[create replace]
       def index
         line_foods = LineFood.active
         if line_foods.exists?
@@ -16,13 +15,29 @@ module Api
           render json: {}, status: :no_content
         end
       end
-      
+
       def create
         if LineFood.active.other_restaurant(@ordered_food.restaurant.id).exists?
           return render json: {
             existing_restaurant: LineFood.other_restaurant(@ordered_food.restaurant.id).first.restaurant.name,
             new_restaurant: Food.find(params[:food_id]).restaurant.name,
           }, status: :not_acceptable
+        end
+
+        set_line_food(@ordered_food)
+
+        if @line_food.save
+          render json: {
+            line_food: @line_food
+          }, status: :created
+        else
+          render json: {}, status: :internal_server_error
+        end
+      end
+
+      def replace
+        LineFood.active.other_restaurant(@ordered_food.restaurant.id).each do |line_food|
+          line_food.update_attribute(:active, false)
         end
 
         set_line_food(@ordered_food)
